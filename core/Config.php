3d0b6f5588d0604cb361594dc47d0759c09d9c8c
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2021-01-18 15:56:00 -0300

    atualizacao atualizacao config/core

--- core/Config.php
+++ core/Config.php
@@ -2,19 +2,7 @@
 session_start();
 ob_start();
 
-define('URL', 'http://localhost/dev/');
-define('URLADM', 'http://localhost/devUsuario/adm/');
-
-define('CONTROLER', 'Home');
-define('METODO', 'index');
-
-//Credenciais de acesso ao BD
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASS', '');
-define('DBNAME', 'devUsuario');
-
-/*define('URL', 'https://projetosdev.info/usuarios/');
+define('URL', 'https://projetosdev.info/usuarios/');
 define('URLADM', 'https://projetosdev.info/usuarios/');
 
 define('CONTROLER', 'Home');
@@ -24,5 +12,5 @@ define('METODO', 'index');
 define('HOST', 'localhost');
 define('USER', 'u224083565_devusuario');
 define('PASS', 'Dev#1449#');
-define('DBNAME', 'u224083565_usuarios');*/
+define('DBNAME', 'u224083565_usuarios');
 
